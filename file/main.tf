d2f0d407b79e663989500f89bb6da321b111748c
vithal <vithalmangaadi@gmail.com>
2024-06-28 19:08:09 +0530

    update1

---
+++ file/main.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "remote-exec" {
+    connection {
+        type="ssh"
+        user=var.user_name
+        agent=false
+        host=var.ec2_public_ip
+        private_key=file(var.ec2_pem_path)
+    }
+
+    provisioner "file" {
+       source=var.source_path
+       destination=var.destination_path
+    }
+}
